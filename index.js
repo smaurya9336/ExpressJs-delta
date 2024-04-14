5f89c02c253b68e54a3f68922d45da14bfb5b155
Sarita <sm7767119@gmail.com>
2024-04-14 12:45:52 +0530

    express js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const req = require("express/lib/request");
+// const req = require("express/lib/request");
 const app = express();
 // console.dir(app);
 
@@ -8,8 +8,30 @@ app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
 
-app.use((req, res) => {
-  //   console.log(req);
-  console.log("request received");
-  res.send("this is a basic response");
+app.get("/", (req, res) => {
+  res.send("you contacted root path");
 });
+
+app.get("/search", (req, res) => {
+  res.send("you contacted search path");
+});
+
+app.get("/home", (req, res) => {
+  res.send("you contacted home path");
+});
+
+app.get("*", (req, res) => {
+  res.send("this path does not exist");
+});
+
+// app.use((req, res) => {
+//   // console.log(req);
+//   console.log("request received");
+//   // res.send("this is a basic response");
+//   // res.send({
+//   //   name: "apple",
+//   //   color: "red",
+//   // });
+//   let code = "<h1>Fruits</h1><ul><li>Apple</li><li>orange</li></ul>";
+//   res.send(code);
+// });
